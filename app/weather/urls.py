ffc55c9e2fbb9a197a9c6ae1cbcfee00261cc0fa
alekseybeloded <belod-aleksej@yandex.ru>
2024-07-20 20:06:37 +0300

    feat: add choose_city path

--- app/weather/urls.py
+++ app/weather/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from weather import views
 
 urlpatterns = [
-    path('', views.get_weather, name='home'),
+    path('', views.get_weather, name='get_weather'),
+    path('choose-city/', views.choose_city, name='choose_city'),
 ]
