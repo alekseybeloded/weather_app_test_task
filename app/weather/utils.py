a1bfdc3b4e5f3af80e47b37ef95cca1973dbee46
alekseybeloded <belod-aleksej@yandex.ru>
2024-07-20 11:24:30 +0300

    feat: add function to get response from API

---
+++ app/weather/utils.py
@@ -0,0 +1,30 @@
+import requests
+from django.http import JsonResponse
+
+menu = [
+    {'title': 'Choose other city', 'url_name': 'home'},
+]
+
+
+def weather_by_city(city_name):
+    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    params = {
+        'key': 'a9e8d05689e64f64bbe142520241807',
+        'q': city_name,
+        'format': 'json',
+        'num_of_days': 1,
+        'lang': 'ru',
+    }
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except (IndexError, TypeError):
+                    return False
+    except (requests.RequestException):
+        return False
+    return JsonResponse(weather)
